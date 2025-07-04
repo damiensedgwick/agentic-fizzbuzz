7518f2f64486806a81388854f9d9df024d552292
Damien Sedgwick <damienksedgwick@gmail.com>
2025-07-04 12:49:23 +0100

    wip: bus messaging working but agent issues are apparent

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
+import { EventEmitter } from "node:events";
 import ora from "ora";
 import {
   fizzbuzzAgent,
   inputValidatorAgent,
+  managerAgent,
   resultValidatorAgent,
 } from "@/agents";
 
@@ -14,20 +16,75 @@ if (!userInput) {
 
 const run = async ({ userInput }: { userInput: string }) => {
   const spinner = ora({
-    text: "🤔",
+    text: "👋 User input received...",
     color: "black",
   }).start();
 
-  const inputValidatorResult = await inputValidatorAgent(userInput);
-  spinner.text = `🔍 Validating input...`;
+  const bus = new EventEmitter();
 
-  const fizzbuzzResult = await fizzbuzzAgent(userInput);
-  spinner.text = `🔍 Fizzbuzzing...`;
+  bus.on("user-input-received", async () => {
+    spinner.text = "📮 Messaging agents...";
+    const nextAgentName = await managerAgent(userInput);
+    bus.emit("task-manager-event", nextAgentName);
+  });
 
-  const resultValidatorResult = await resultValidatorAgent(fizzbuzzResult);
-  spinner.text = `🔍 Validating result...`;
+  bus.on("task-manager-event", async (nextAgentName: string) => {
+    spinner.text = `🤔 Activating ${nextAgentName}...`;
 
-  spinner.succeed(`🎉 FizzBuzz result: ${fizzbuzzResult}`);
+    switch (nextAgentName) {
+      case "input-validator": {
+        spinner.text = "🔍 Validating input...";
+        const inputValidatorResult = await inputValidatorAgent(userInput);
+        bus.emit("input-validator-event", inputValidatorResult);
+        break;
+      }
+      case "fizzbuzz": {
+        spinner.text = "🔍 Fizzbuzzing...";
+        const fizzbuzzResult = await fizzbuzzAgent(userInput);
+        bus.emit("fizzbuzz-event", fizzbuzzResult);
+        break;
+      }
+      case "result-validator": {
+        spinner.text = "🔍 Validating result...";
+        const resultValidatorResult = await resultValidatorAgent(userInput);
+        bus.emit("result-validator-event", resultValidatorResult);
+        break;
+      }
+      case "error-message": {
+        spinner.text = "🚨 Error message...";
+        bus.emit("error-message-event", "Invalid input");
+        break;
+      }
+    }
+  });
+
+  bus.on("input-validator-event", async (inputValidatorResult: boolean) => {
+    if (inputValidatorResult) {
+      bus.emit("task-manager-event", "fizzbuzz");
+    }
+
+    bus.emit("task-manager-event", "error-message");
+  });
+
+  bus.on("fizzbuzz-event", async (fizzbuzzResult: string) => {
+    bus.emit("task-manager-event", "result-validator", fizzbuzzResult);
+  });
+
+  bus.on("result-validator-event", async (resultValidatorResult: boolean) => {
+    if (resultValidatorResult) {
+      bus.emit("task-manager-event", "result-printer");
+    }
+  });
+
+  bus.on("result-printer-event", async (resultPrinterResult: string) => {
+    spinner.succeed(`🎉 FizzBuzz result: ${resultPrinterResult}`);
+  });
+
+  bus.on("error-message-event", async (errorMessage: string) => {
+    spinner.fail(`🚨 Error: ${errorMessage}`);
+  });
+
+  bus.emit("user-input-received");
 };
 
 await run({ userInput });
--- src/agents.ts
+++ src/agents.ts
@@ -1,5 +1,14 @@
 import { generateText } from "ai";
 import { openai } from "@/lib/openai";
+import { fizzbuzz } from "./lib/fizzbuzz";
+
+const agentNames = [
+  "input-validator",
+  "fizzbuzz",
+  "result-validator",
+  "result-printer",
+  "error-message",
+];
 
 export const managerAgent = async (input: string) => {
   const { text } = await generateText({
@@ -14,6 +23,9 @@ export const managerAgent = async (input: string) => {
         - If the result validator agent returns a valid result, you will need to pass it to the result printer agent.
         - If the result validator agent returns an invalid result, you will need inform the error message agent to inform the user of the error.
         - The user input is ${input}.
+        - The next agent to be activated must be called by the name of the agent.
+        - The agent names are: ${agentNames.join(", ")}.
+        - You must return the name of the next agent to be activated.
         `,
   });
 
@@ -24,17 +36,18 @@ export const inputValidatorAgent = async (input: string) => {
   const { text } = await generateText({
     model: openai("o3-mini"),
     prompt: `
-        You are a helpful assistant that can help with FizzBuzz input validation.
+        You are a helpful assistant that can help with FizzBuzz user input validation.
         
         - The user has provided a number and you need to validate it.
         - The number is ${input}.
-        - The result should be a boolean.
-        - If the number is not a number, you should return false.
-        - If the number is a number, you should return true.
+        - The number should be a positive integer.
+        - The number should be between 1 and 100.
+        - If the number is not a positive integer or is not between 1 and 100, you should return false.
+        - If the number is a positive integer and is between 1 and 100, you should return true.
         `,
   });
 
-  return text;
+  return Boolean(text);
 };
 
 export const fizzbuzzAgent = async (input: string) => {
@@ -48,6 +61,7 @@ export const fizzbuzzAgent = async (input: string) => {
         - The result should be a string.
         `,
   });
+  console.log("🚀 ~ fizzbuzzAgent ~ text:", text);
 
   return text;
 };
@@ -58,13 +72,16 @@ export const resultValidatorAgent = async (input: string) => {
     prompt: `
         You are a helpful assistant that can help with FizzBuzz result validation.
 
-        - The user has provided a number and you need to validate the result.
+        - The user has provided a number and you need to validate the generated result.
         - The result is ${input}.
-        - The result should be a boolean.
-        - If the result is not a string, you should return false.
-        - If the result is a string, you should return true.
+        - The result should be a string.
+        - You should use fizzbuzz.ts to validate the result and return the result.
+        - The result can be Fizz, Buzz, FizzBuzz or the number itself.
         `,
   });
+  console.log("🚀 ~ resultValidatorAgent ~ text:", text);
 
-  return text;
+  const result = fizzbuzz(Number(input));
+
+  return result === text;
 };
